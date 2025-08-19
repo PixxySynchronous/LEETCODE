b1d8e7aba5d942e36f7cba4413c03ece4c1711eb
PixxySynchronous <theamazingpiyush@gmail.com>
2025-08-19 23:18:16 +0530

    19th august 2025

--- DSA-NO-DOUBT/Product-sum.cpp
+++ DSA-NO-DOUBT/Product-sum.cpp
@@ -1,4 +1,4 @@
-// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+// Leetcode 1281 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
 class Solution {
 public:
