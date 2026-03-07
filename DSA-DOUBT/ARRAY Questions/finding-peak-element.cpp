62d2b9e66ef23bea73c28a5366a9355d4059d295
Piyush Singh <theamazingpiyush@gmail.com>
2026-03-08 00:44:24 +0530

    Optimizing codes

--- DSA-DOUBT/ARRAY Questions/finding-peak-element.cpp
+++ DSA-DOUBT/ARRAY Questions/finding-peak-element.cpp
@@ -4,34 +4,21 @@
 
 // Your task is to solve it in O(log(n)) time complexity.
 
-int findpeak(vector<int>& arr){
-    int s=0;
-    int e=arr.size()-1;
-    int ans = 0;
-    int mid =(s + (e-s)/2);
-    while (s<=e){
-        if (mid > 0 && mid < arr.size() - 1 && arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
-            ans=mid;
-            break;  
-        }
-        else if (mid > 0 && arr[mid-1]>arr[mid]){
-            e=mid-1;
-        }
-        else if (mid < arr.size() - 1 && arr[mid+1]>arr[mid]){
-            s=mid+1; 
-        }
-        mid = (s +(e-s)/2); 
-    }
-    return ans; 
-}
-
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-    int ans = findpeak( arr);
-    return ans; 
-        
+        int s = 0 ;
+        int end = arr.size()-1;
+        int mid = s + (end-s) /2;
+        while ( s < end){
+            if (arr[mid] < arr[mid+1]){
+                s= mid +1; 
+            }
+            else 
+            end = mid; 
+            mid = s + (end-s) /2;
+        }
+        return mid; 
     }
 };
 
-//the whole solution was thought of by me. Since i knew, however, to use binary search, it made the time complexity constraint practically useless. I needed help with the out of bounds cases via gpt as i did not think of them myself. ANd also the break line was essential as at that time we had already found the answer. That too was given by gpt.
